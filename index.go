a39ff10538cdcece5feb7e84ec9f4c58e8ef47d8
zackyymughnii <113091095+zackyymughnii@users.noreply.github.com>
2023-04-28 10:52:49 +0700

    Added Redocly template

--- index.go
+++ index.go
@@ -1,6 +1,6 @@
 package swagger
 
-const indexTmpl string = `
+const indexTmplBackup string = `
 <!-- HTML for static distribution bundle build -->
 <!DOCTYPE html>
 <html lang="en">
@@ -98,4 +98,32 @@ const indexTmpl string = `
     </script>
   </body>
 </html>
+`
+
+const indexTmpl string = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{{.Title}}</title>
+    <!-- needed for adaptive design -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+
+    <!--
+    Redoc doesn't change outer page styles
+    -->
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <redoc spec-url='{{.URL}}'></redoc>
+    <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"> </script>
+  </body>
+</html>
+
 `
